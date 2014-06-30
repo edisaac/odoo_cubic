42e26c0d17edc1ea071629dfca7a711a4ca0d158
Developer Team <info@cubicerp.com>
2014-06-29 21:32:35 -0500

    [FIX] purchase averange price without taxes

--- addons/purchase/purchase.py
+++ addons/purchase/purchase.py
@@ -643,6 +643,11 @@ class purchase_order(osv.osv):
         }
 
     def _prepare_order_line_move(self, cr, uid, order, order_line, picking_id, context=None):
+        cur_obj=self.pool.get('res.currency')
+        tax_obj = self.pool.get('account.tax')
+        taxes = tax_obj.compute_all(cr, uid, order_line.taxes_id, order_line.price_unit, 1, order_line.product_id, order_line.order_id.partner_id)
+        cur = order_line.order_id.pricelist_id.currency_id
+        price_unit = cur_obj.round(cr, uid, cur, taxes['total'])
         return {
             'name': order_line.name or '',
             'product_id': order_line.product_id.id,
@@ -661,7 +666,7 @@ class purchase_order(osv.osv):
             'type':'in',
             'purchase_line_id': order_line.id,
             'company_id': order.company_id.id,
-            'price_unit': order_line.price_unit
+            'price_unit': price_unit
         }
 
     def _create_pickings(self, cr, uid, order, order_lines, picking_id=False, context=None):
