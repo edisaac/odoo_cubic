affd937305bbb6451ca1fd38a6b679ac7c5b3d91
Developer Team <info@cubicerp.com>
2015-02-18 01:03:51 -0500

    Fix robots.txt and sitemap.xml with web.base.url param

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -74,7 +74,8 @@ class Website(openerp.addons.web.controllers.main.Home):
 
     @http.route(['/robots.txt'], type='http', auth="public")
     def robots(self):
-        return request.render('website.robots', {'url_root': request.httprequest.url_root}, mimetype='text/plain')
+        url = request.registry['ir.config_parameter'].get_param(request.cr, openerp.SUPERUSER_ID, 'web.base.url') or request.httprequest.url_root
+        return request.render('website.robots', {'url_root': url+'/'}, mimetype='text/plain')
 
     @http.route('/sitemap.xml', type='http', auth="public", website=True)
     def sitemap_xml_index(self):
@@ -83,6 +84,13 @@ class Website(openerp.addons.web.controllers.main.Home):
         iuv = request.registry['ir.ui.view']
         mimetype ='application/xml;charset=utf-8'
         content = None
+        url_root = request.registry['ir.config_parameter'].get_param(request.cr, openerp.SUPERUSER_ID, 'web.base.url')
+        if url_root:
+            url_root = url_root + '/'
+        else:
+            url_root =  request.httprequest.url_root
+            
+        
 
         def create_sitemap(url, content):
             ira.create(cr, uid, dict(
@@ -115,7 +123,7 @@ class Website(openerp.addons.web.controllers.main.Home):
                 start = pages * LOC_PER_SITEMAP
                 values = {
                     'locs': islice(locs, start, start + LOC_PER_SITEMAP),
-                    'url_root': request.httprequest.url_root[:-1],
+                    'url_root': url_root[:-1],
                 }
                 urls = iuv.render(cr, uid, 'website.sitemap_locs', values, context=context)
                 if urls.strip():
@@ -134,7 +142,7 @@ class Website(openerp.addons.web.controllers.main.Home):
                 # Sitemaps must be split in several smaller files with a sitemap index
                 content = iuv.render(cr, uid, 'website.sitemap_index_xml', dict(
                     pages=range(1, pages + 1),
-                    url_root=request.httprequest.url_root,
+                    url_root=url_root,
                 ), context=context)
             create_sitemap('/sitemap.xml', content)
 
