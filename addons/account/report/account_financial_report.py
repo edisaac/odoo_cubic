98244f959eea475732ca3a40d8f42bdaeaf1867c
Cubic ERP <info@cubicerp.com>
2016-07-22 15:48:36 -0400

    [IMP] account_financial_report

--- addons/account/report/account_financial_report.py
+++ addons/account/report/account_financial_report.py
@@ -78,7 +78,40 @@ class report_account_common(report_sxw.rml_parse, common_report_header):
                 account_ids = account_obj._get_children_and_consol(self.cr, self.uid, [x.id for x in report.account_ids])
             elif report.type == 'account_type' and report.account_type_ids:
                 account_ids = account_obj.search(self.cr, self.uid, [('user_type','in', [x.id for x in report.account_type_ids])])
-            if account_ids:
+            if report.type == 'account_type' and report.display_detail == 'detail_flat' and report.account_type_ids:
+                for report_type in report.account_type_ids:
+                    flag = False
+                    balance = debit = credit = balance_cmp= 0
+                    account_type = 'other'
+                    account = False
+                    for account in account_obj.browse(self.cr, self.uid, account_obj.search(self.cr, self.uid, [('user_type','=',report_type.id)]),
+                                                      context=data['form']['used_context']):
+                        balance += account.balance
+                        debit += account.debit
+                        credit += account.credit
+                        account_type = account.type
+                        if data['form']['enable_filter']:
+                            balance_cmp += account_obj.browse(self.cr, self.uid, account.id, context=data['form']['comparison_context']).balance
+                    vals = {
+                        'name': report_type.name,
+                        'balance': balance != 0 and balance * report.sign or balance,
+                        'type': 'account',
+                        'level': 6,
+                        'account_type': account_type,
+                    }
+                    if data['form']['debit_credit']:
+                        vals['debit'] = debit
+                        vals['credit'] = credit
+                    if account and not currency_obj.is_zero(self.cr, self.uid, account.company_id.currency_id, vals['balance']):
+                        flag = True
+                    if data['form']['enable_filter']:
+                        vals['balance_cmp'] = balance_cmp * report.sign or 0.0
+                        if account and not currency_obj.is_zero(self.cr, self.uid, account.company_id.currency_id,
+                                                    vals['balance_cmp']):
+                            flag = True
+                    if flag:
+                        lines.append(vals)
+            elif account_ids:
                 for account in account_obj.browse(self.cr, self.uid, account_ids, context=data['form']['used_context']):
                     #if there are accounts to display, we add them to the lines with a level equals to their level in
                     #the COA + 1 (to avoid having them with a too low level that would conflicts with the level of data
