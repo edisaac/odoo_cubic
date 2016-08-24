be2ea7f4d213e835dbf16aecfa8fe39ea1f3cef5
Cubic ERP <info@cubicerp.com>
2016-08-24 14:25:29 -0500

    [FIX] mrp new order without procurement

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -1130,7 +1130,7 @@ class mrp_production(osv.osv):
             'production_id': production.id,
             'origin': production.name,
             'group_id': procurement and procurement.group_id.id or production.group_id.id,
-            'property_ids': [(6,False, [p.id for p in procurement.property_ids or production.property_ids])],
+            'property_ids': procurement and [(6,False, [p.id for p in procurement.property_ids or production.property_ids])] or [],
         }
         move_id = stock_move.create(cr, uid, data, context=context)
         #a phantom bom cannot be used in mrp order so it's ok to assume the list returned by action_confirm
@@ -1246,7 +1246,7 @@ class mrp_production(osv.osv):
         procs = proc_obj.search(cr, uid, [('production_id', '=', production.id)], context=context)
         procurement = procs and\
             proc_obj.browse(cr, uid, procs[0], context=context) or False
-        for property in procurement.property_ids or production.property_ids:
+        for property in procurement and procurement.property_ids or production.property_ids:
             if property.group_id in product.property_group_ids:
                 res.append(property.id)
         return res
